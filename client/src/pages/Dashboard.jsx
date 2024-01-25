aaa0b41a1d8b0b8236d5c05f095525e9ea5cd5c7
Samir Sadek <samirsadek2009@gmail.com>
2024-01-25 09:51:32 +0600

    dashboard Sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashboardSidebar from "../components/DashboardSidebar";
+import DashboardProfile from "../components/DashboardProfile";
 
 const Dashboard = () => {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search)
+        const tabFromUrl = urlParams.get('tab')
+        if(tabFromUrl){
+            setTab(tabFromUrl)
+        }
+    },[location.search])
     return (
-        <div>
-            Dashboard
+        <div className="min-h-screen flex flex-col md:flex-row">
+            <div className='md:w-56'>
+                <DashboardSidebar/>
+            </div>
+            
+                {
+                    tab === 'profile'  && <div><DashboardProfile/></div>
+                }
+            
         </div>
     );
 };
